c4f50077d754c42b6ae7b9568d099b8d468a04ff
Jorge <joravasal@gmail.com>
2015-09-07 16:27:36 +0300

    Improved drawing and second counter

--- src/main.c
+++ src/main.c
@@ -3,10 +3,6 @@
 #include "pebble-assist.h"
 #include "numbers.h"
 
-// After dividing the screen on squares with 14px per side,
-//there are 4 pixels left on the width
-#define W_OFFSET 2
-
 Window *my_window;
 static Layer *bg_layer;
 
@@ -14,80 +10,106 @@ static int min10;
 static int min1;
 static int hour10;
 static int hour1;
+static int sec;
 
+static bool is_bt_connected;
+static int batt_level;
+
+static bool is_bt_shown = false;
+static bool is_batt_shown = false;
+static bool is_sec_shown = false;
+
+// This adds a number of steps every time the time changes.
 static const int maxNumMiddleAnimation = 1;
-static int numMiddleAnimation;
+static int numMiddleAnimation = 1;
+// which shape is used next
 static int middleShape = 0;
 
 AppTimer *animation_timer; //for the animation
 const int timer_delay = 25;
 
-static GPoint array_hour_10[NUMBER_HEIGHT][NUMBER_WIDTH] = {
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-};
+//Stores the angle of each spock in the screen (120).
+static GPoint spocks[SPOCKS_SCREEN_HEIGHT][SPOCKS_SCREEN_WIDTH];
+static GPoint SPOCKS_CENTERS[SPOCKS_SCREEN_HEIGHT][SPOCKS_SCREEN_WIDTH];
 
-static GPoint array_hour_1[NUMBER_HEIGHT][NUMBER_WIDTH] = {
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-};
+static void init_centers_array() {
+  for (int i = 0; i < SPOCKS_SCREEN_HEIGHT; i++) {
+    for (int j = 0; j < SPOCKS_SCREEN_WIDTH; j++) {
+      GPoint p = {
+        .x = RADIUS_SPOCK * 2 * j + RADIUS_SPOCK,
+        .y = RADIUS_SPOCK * 2 * i + RADIUS_SPOCK,
+      };
+      SPOCKS_CENTERS[i][j] = p;
+    }
+  }
+}
 
-static GPoint array_minute_10[NUMBER_HEIGHT][NUMBER_WIDTH] = {
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-};
+static void init_spocks_array() {
+  for (int i = 0; i < SPOCKS_SCREEN_HEIGHT; i++) {
+    for (int j = 0; j < SPOCKS_SCREEN_WIDTH; j++) {
+      GPoint p = {
+        .x = 0,
+        .y = 180,
+      };
+      spocks[i][j] = p;
+    }
+  }
+}
 
-static GPoint array_minute_1[NUMBER_HEIGHT][NUMBER_WIDTH] = {
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-  {{0, 180}, {0, 180}, {0, 180}, {0, 180},},
-};
-
-static bool draw_array(GContext *ctx, GPoint array[NUMBER_HEIGHT][NUMBER_WIDTH],
-                       const GPoint array_obj[NUMBER_HEIGHT][NUMBER_WIDTH],
-                       int offset_w, int offset_h) {
+// Draws the array of spocks from the initial at coordinates (param_x_start, param_y_start)
+//a rectangle with param_width columns and param_height rows.
+//If any value is illegal (negative or out of bounds) it will use the default:
+//  for the starting position, (0,0)
+//  for the width and height, until the end of the array.
+//
+//Returns a boolean if there was any *relevant* change made.
+//***The model used as comparison may have irrelevant areas,
+//   those spocks that are ok in any direction.
+//
+// Examples: draw_array(ctx, -1, -1, -1, -1, array, size_w, size_h); will draw in the full screen.
+//          draw_array(ctx, -1, -1, 3, 3, array, size_w, size_h); will draw in a 3x3 square from the first item.
+//          draw_array(ctx, 8, 0, -1, -1, array, size_w, size_h); will draw in the last two columns.
+static bool draw_array(GContext *ctx, int param_x_start, int param_y_start,
+                       int param_width, int param_height,
+                       GPoint *array_objective,
+                       int obj_width, int obj_height) {
   LOG("Drawing an array of tiny clocks");
-  bool equals = true;
-  for(int i = 0; i < NUMBER_HEIGHT; i++) {
-    for(int j = 0; j < NUMBER_WIDTH; j++) {
-      //DEBUG("Step i = %d - j = %d", i, j);
-      GPoint center = NUMBER_CENTERS[i][j];
-      center.x = center.x + offset_w + W_OFFSET;
-      center.y = center.y + offset_h;
+  int x_start = 0;
+  if(param_x_start > 0 && param_x_start < SPOCKS_SCREEN_WIDTH) x_start = param_x_start;
+  int y_start = 0;
+  if(param_y_start > 0 && param_y_start < SPOCKS_SCREEN_HEIGHT) y_start = param_y_start;
+  int width = SPOCKS_SCREEN_WIDTH - x_start;
+  if(param_width > 0 && param_width + x_start <= SPOCKS_SCREEN_WIDTH) width = param_width + x_start;
+  int height = SPOCKS_SCREEN_HEIGHT - y_start;
+  if(param_height > 0 && param_height + y_start <= SPOCKS_SCREEN_HEIGHT) height = param_height + y_start;
+  
+  bool modified = false;
+  for(int i = y_start; i < height; i++) {
+    for(int j = x_start; j < width; j++) {
+      GPoint center = SPOCKS_CENTERS[i][j];
+      center.x = center.x + W_OFFSET;
+      //center.y = center.y + H_OFFSET; //Unnecessary at the moment
       
-      int32_t angle1 = array[i][j].x;
-      int32_t angle2 = array[i][j].y;
-      if(array_obj != NULL) {
-        if(array_obj[i][j].x == -1) {
+      int32_t angle1 = spocks[i][j].x;
+      int32_t angle2 = spocks[i][j].y;
+      if(array_objective != NULL) {
+        int obj_pos = ((i - y_start) % obj_height) * obj_width + ((j - x_start) % obj_width);
+        LOG("obj_pos=%d", obj_pos);
+        if(array_objective[obj_pos].x == -1) {
           angle1 = (angle1 + ANGLE_STEP) % 360;
-          array[i][j].x = angle1;
+          spocks[i][j].x = angle1;
           angle2 = (angle2 + ANGLE_STEP) % 360;
-          array[i][j].y = angle2;
+          spocks[i][j].y = angle2;
         } else {
-          if (array_obj[i][j].x != angle1) {
+          if (array_objective[obj_pos].x != angle1) {
             angle1 = (angle1 + ANGLE_STEP) % 360;
-            array[i][j].x = angle1;
-            equals = false;
+            spocks[i][j].x = angle1;
+            modified = true;
           }
-          if (array_obj[i][j].y != angle2) {
+          if (array_objective[obj_pos].y != angle2) {
             angle2 = (angle2 + ANGLE_STEP) % 360;
-            array[i][j].y = angle2;
-            equals = false;
+            spocks[i][j].y = angle2;
+            modified = true;
           }
         }
       }
@@ -95,12 +117,12 @@ static bool draw_array(GContext *ctx, GPoint array[NUMBER_HEIGHT][NUMBER_WIDTH],
       angle2 = TRIG_MAX_ANGLE * angle2 / 360;
       
       GPoint point1 = {
-        .x = (int16_t)(cos_lookup(angle1) * RADIUS / TRIG_MAX_RATIO) + center.x,
-        .y = (int16_t)(-sin_lookup(angle1) * RADIUS / TRIG_MAX_RATIO) + center.y,
+        .x = (int16_t)(cos_lookup(angle1) * LENGTH_HAND_SPOCK / TRIG_MAX_RATIO) + center.x,
+        .y = (int16_t)(-sin_lookup(angle1) * LENGTH_HAND_SPOCK / TRIG_MAX_RATIO) + center.y,
       };
       GPoint point2 = {
-        .x = (int16_t)(cos_lookup(angle2) * RADIUS / TRIG_MAX_RATIO) + center.x,
-        .y = (int16_t)(-sin_lookup(angle2) * RADIUS / TRIG_MAX_RATIO) + center.y,
+        .x = (int16_t)(cos_lookup(angle2) * LENGTH_HAND_SPOCK / TRIG_MAX_RATIO) + center.x,
+        .y = (int16_t)(-sin_lookup(angle2) * LENGTH_HAND_SPOCK / TRIG_MAX_RATIO) + center.y,
       };
       
       graphics_context_set_stroke_color(ctx, GColorBlack);
@@ -109,58 +131,151 @@ static bool draw_array(GContext *ctx, GPoint array[NUMBER_HEIGHT][NUMBER_WIDTH],
       
     }
   }
-  return equals;
+  return modified;
 }
 
-static void update_time() {
+static bool update_time() {
   LOG("Updating time");
   time_t now = time(NULL);
   struct tm *time = localtime(&now);
   int hour = time->tm_hour;
+  int new_min = -1;
+  int new_sec = -1;
+  bool res = false;
 
-  min10 = time->tm_min / 10;
-  min1 = time->tm_min % 10;
   if(clock_is_24h_style() == false && hour > 12) {
     hour = hour - 12;
   }
   hour10 = hour / 10;
   hour1 = hour % 10;
   
+  if (min10 != time->tm_min / 10) {
+    middleShape = rand() % NUM_SHAPES;
+    numMiddleAnimation = maxNumMiddleAnimation;
+  }
+  min10 = time->tm_min / 10;
+  
+  new_min = time->tm_min % 10;
+  if(min1 != new_min) {
+    min1 = new_min;
+    res = true;
+  }
+  
+  if(is_sec_shown) {
+    new_sec = time->tm_sec % 5;
+    new_sec = (time->tm_sec - new_sec) / 5;
+    if (sec != new_sec) {
+      sec = new_sec;
+      res = true;
+    }
+  }
+  
   DEBUG("Time is %d%d:%d%d", hour10, hour1, min10, min1);
+  return res;
 }
 
 static void tick_handler(struct tm *time, TimeUnits units_changed) {
   LOG("tick-tock");
-  update_time();
-  middleShape = rand() % NUM_SHAPES;
-  layer_mark_dirty(window_get_root_layer(my_window));
+  if (update_time())
+    layer_mark_dirty(window_get_root_layer(my_window));
+}
+
+static void bt_handler(bool connected) {
+  LOG("Bluetooth connection changed: %d", connected);
+  is_bt_connected = connected;
+  if(is_bt_shown) {
+    layer_mark_dirty(window_get_root_layer(my_window));
+  }
+  /*if(connected == true) {
+  }
+  else {
+  }*/
 }
 
 void animation_callback(void *data) {
-  LOG("Animation callback");
+  //LOG("Animation callback");
   layer_mark_dirty(window_get_root_layer(my_window));
 }
 
 static void bg_update_proc(Layer *layer, GContext *ctx) {
   LOG("Updating background layer");
+  //Boolean to help with the animation
   bool stop_animation = true;
-  stop_animation = draw_array(ctx, array_hour_10, (numMiddleAnimation > 0 ? SHAPES[middleShape] : NUMBERS[hour10]), 0, 0) && stop_animation;
-  stop_animation = draw_array(ctx, array_hour_1, (numMiddleAnimation > 0 ? SHAPES[middleShape] : NUMBERS[hour1]), NUMBER_WIDTH*DIAMETER, 0) && stop_animation;
-  stop_animation = draw_array(ctx, array_minute_10, (numMiddleAnimation > 0 ? SHAPES[middleShape] : NUMBERS[min10]), 0, NUMBER_HEIGHT*DIAMETER) && stop_animation;
-  stop_animation = draw_array(ctx, array_minute_1, (numMiddleAnimation > 0 ? SHAPES[middleShape] : NUMBERS[min1]), NUMBER_WIDTH*DIAMETER, NUMBER_HEIGHT*DIAMETER) && stop_animation;
+  
+  if(numMiddleAnimation > 0) {
+    // Draw the full screen with some geometric pattern
+    stop_animation = !draw_array(ctx, -1, -1, -1, -1, SHAPES2X2[middleShape], 2, 2) && stop_animation;
+  } else {
+    //Updating the time (hours and minutes)
+    stop_animation = !draw_array(ctx, 1, 0, NUMBER_WIDTH, NUMBER_HEIGHT,
+                                 NUMBERS[hour10], NUMBER_WIDTH, NUMBER_HEIGHT) && stop_animation;
+    
+    stop_animation = !draw_array(ctx, NUMBER_WIDTH + 1, 0, NUMBER_WIDTH, NUMBER_HEIGHT,
+                                 NUMBERS[hour1], NUMBER_WIDTH, NUMBER_HEIGHT) && stop_animation;
+    
+    stop_animation = !draw_array(ctx, 1, NUMBER_HEIGHT, NUMBER_WIDTH, NUMBER_HEIGHT,
+                                 NUMBERS[min10], NUMBER_WIDTH, NUMBER_HEIGHT) && stop_animation;
+    
+    stop_animation = !draw_array(ctx, NUMBER_WIDTH + 1, NUMBER_HEIGHT, NUMBER_WIDTH, NUMBER_HEIGHT,
+                                 NUMBERS[min1], NUMBER_WIDTH, NUMBER_HEIGHT) && stop_animation;
+    
+    //Updating the seconds
+    if(is_sec_shown) {
+      //Corner at the beginning
+      stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, SPOCKS_SCREEN_HEIGHT - 1, 1, 1, SECONDS1X1[0], 1, 1) && stop_animation;
+      for(int i = 1; i <= sec; i++) {
+        //Loop for the middle steps
+        stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, SPOCKS_SCREEN_HEIGHT - 1 - i, 1, 1, SECONDS1X1[1], 1, 1) && stop_animation;
+      }
+      if(sec < SPOCKS_SCREEN_HEIGHT - 1) {
+        //Corner at the end
+        stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, SPOCKS_SCREEN_HEIGHT - 2 - sec, 1, 1, SECONDS1X1[2], 1, 1) && stop_animation;
+        for(int i = sec + 2; i < SPOCKS_SCREEN_HEIGHT; i++) {
+          //Loop for the rest
+          stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, SPOCKS_SCREEN_HEIGHT - 1 - i, 1, 1, SECONDS1X1[3], 1, 1) && stop_animation;
+        }
+      }
+    } else {
+      stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, 0, 1, -1, NULL, 0, 0) && stop_animation;
+    }
+    
+    //Updating the bluetooth status
+    if(is_bt_shown) {
+      stop_animation = !draw_array(ctx, 0, 0, 1, 2, is_bt_connected ? BLUETOOTH2X1[1] : BLUETOOTH2X1[0], 2, 1) && stop_animation;
+    } else {
+      stop_animation = !draw_array(ctx, 0, 0, 1, 2, NULL, 0, 0) && stop_animation;
+    }
+    
+    //Updating the battery level
+    if(is_batt_shown) {
+      stop_animation = !draw_array(ctx, 0, 0, 1, -1, NULL, 0, 0) && stop_animation;
+    } else {
+      stop_animation = !draw_array(ctx, 0, 0, 1, -1, NULL, 0, 0) && stop_animation;
+    }
+    
+  }
+  
   if(!stop_animation || numMiddleAnimation > 0) {
     if (stop_animation) {
       numMiddleAnimation--;
       middleShape = rand() % NUM_SHAPES;
     }
     animation_timer = app_timer_register(timer_delay, (AppTimerCallback) animation_callback, NULL);
-  } else {
-    numMiddleAnimation = maxNumMiddleAnimation;
   }
 }
 
 static void window_load(Window *window) {
   LOG("Window load");
+  is_bt_shown = true; //grab from mem
+  is_batt_shown = false; //grab from mem
+  is_sec_shown = false; //grab from mem
+  
+  // Register with TickTimerService
+  if(is_sec_shown) 
+    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  else
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
   Layer *window_layer = window_get_root_layer(my_window);
   GRect bounds = layer_get_bounds(window_layer);
 
@@ -188,12 +303,15 @@ void handle_init(void) {
   
   window_stack_push(my_window, true);
   
+  init_centers_array();
+  init_spocks_array();
+  
+  //Subscribe to BluetoothConnectionService
+  bluetooth_connection_service_subscribe(bt_handler);
+  
   // Make sure the time is displayed from the start
   numMiddleAnimation = maxNumMiddleAnimation;
   update_time();
-  
-  // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 void handle_deinit(void) {
--- src/main.h
+++ src/main.h
@@ -2,18 +2,37 @@
 
 #include <pebble.h>
   
-#define RADIUS 7
-#define DIAMETER 14
+// After dividing the screen on squares with 14px per side,
+//there are 4 pixels left on the width
+#define W_OFFSET 2
+//#define H_OFFSET 0 //for completion's sake, maybe needed if the spocks' sizes change.
+
+// The length of the hands of the clocks is quite independent from everything else
+//and can be modified freely, reducing to 5 creates a nice dashed effect, 
+//increasing will make the hands reach further with no effect on the numbers.
+#define LENGTH_HAND_SPOCK 7
+
+// The radius is quite important and it will define how many spocks fit on the screen,
+//by increasing it "resolution" for the numbers is lost so it is not really useful.
+//If decreased, there is more space for spocks and the numbers look smaller, could be
+//interesting to test with a size 5 and see what can fit.
+#define RADIUS_SPOCK 7
+
 #define NUMBER_WIDTH 4
 #define NUMBER_HEIGHT 6
 
+// At the moment these two values are hardcoded although they depend on the RADIUS_SPOCK.
+//With these numbers most of the screen is used, only leaving 4 columns of pixels unused
+//on the width (10 spocks * 14px diameter each = 140px out of 144px).
+//For the height it gives an exact number: 12 spocks * 14px diameter each = 168 px
+#define SPOCKS_SCREEN_WIDTH 10
+#define SPOCKS_SCREEN_HEIGHT 12
+
 #define ANGLE_STEP 5
   
-static const GPoint NUMBER_CENTERS[NUMBER_HEIGHT][NUMBER_WIDTH] = {
-  {{RADIUS, RADIUS,}, {RADIUS * 3, RADIUS,}, {RADIUS * 5, RADIUS,}, {RADIUS * 7, RADIUS,},},
-  {{RADIUS, RADIUS * 3,}, {RADIUS * 3, RADIUS * 3,}, {RADIUS * 5, RADIUS * 3,}, {RADIUS * 7, RADIUS * 3,},},
-  {{RADIUS, RADIUS * 5,}, {RADIUS * 3, RADIUS * 5,}, {RADIUS * 5, RADIUS * 5,}, {RADIUS * 7, RADIUS * 5,},},
-  {{RADIUS, RADIUS * 7,}, {RADIUS * 3, RADIUS * 7,}, {RADIUS * 5, RADIUS * 7,}, {RADIUS * 7, RADIUS * 7,},},
-  {{RADIUS, RADIUS * 9,}, {RADIUS * 3, RADIUS * 9,}, {RADIUS * 5, RADIUS * 9,}, {RADIUS * 7, RADIUS * 9,},},
-  {{RADIUS, RADIUS * 11,}, {RADIUS * 3, RADIUS * 11,}, {RADIUS * 5, RADIUS * 11,}, {RADIUS * 7, RADIUS * 11,},},
-};
+#define HIDE_BLUETOOTH 0
+#define SHOW_BLUETOOTH 1
+#define HIDE_BATTERY 0
+#define SHOW_BATTERY 1
+#define HIDE_SECONDS 0
+#define SHOW_SECONDS 1
