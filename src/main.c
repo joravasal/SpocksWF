78615d135ebb9cbd96d1b25f1775371a1c64ef26
Jorge Avalos <joravasal@gmail.com>
2015-09-12 12:09:58 +0300

    Shake watch to show details, preparing for the configuration options

--- src/main.c
+++ src/main.c
@@ -23,6 +23,22 @@ static bool is_bt_shown = false;
 static bool is_batt_shown = false;
 static bool is_sec_shown = false;
 
+static bool bt_show_on_shake = false;
+static bool batt_show_on_shake = false;
+static bool sec_show_on_shake = false;
+static bool is_shake_active = false;
+static AppTimer *shake_timer;
+static int shake_timer_delay = 30000;
+
+static bool show_zero_for_hours = false;
+
+static int animation_rate = ANIMATE_EVERY_1M;
+// This adds a certain number of shapes to show every once in a while or on loading the watchface.
+static const int MAX_ANIMATIONS = 1;
+static int current_middle_animation = 1;
+// which shape is used next
+static int middle_shape = 0;
+
 static int hand_length = NORMAL_LENGTH_HAND_SPOCK;
 
 // Colors. The pattern can be BACKGROUND_EQUAL, BACKGROUND_DIFF_SIDE_BAR, BACKGROUND_HOUR_MIN and BACKGROUND_ALL_DIFF
@@ -40,17 +56,14 @@ static GColor color_spocks_m1;
 static GColor color_spocks_sec;
 static GColor color_spocks_bt;
 
-// This adds a certain number of shapes to show every ten minutes or on loading the watchface.
-static const int maxNumMiddleAnimation = 1;
-static int numMiddleAnimation = 1;
-// which shape is used next
-static int middleShape = 0;
-
-AppTimer *animation_timer; //for the animation
+static AppTimer *animation_timer; //for the animation
 const int timer_delay = 30;
 
 //Stores the angle of each spock in the screen (120).
 static GPoint spocks[SPOCKS_SCREEN_HEIGHT][SPOCKS_SCREEN_WIDTH];
+//Stores the center point of each Spock, it could be easily calculated on each iteration instead of
+//stored in memory. Since memory shouldn't be a problem, better to save the calculations? It might
+//help save battery, it's 120 times per minute (at least) calculating the same products and additions.
 static GPoint SPOCKS_CENTERS[SPOCKS_SCREEN_HEIGHT][SPOCKS_SCREEN_WIDTH];
 
 static void init_centers_array() {
@@ -191,7 +204,7 @@ static bool draw_array(GContext *ctx, int param_x_start, int param_y_start,
           //make some structural changes into those, this will be affected most likely. As a fast
           //solution it should be good enough for now.
           objective_pos = (i % 2) * 2 + (j % 2);
-          directions = calculate_spocks_hands_direction(i, j, SHAPES2X2[middleShape][objective_pos]);
+          directions = calculate_spocks_hands_direction(i, j, SHAPES2X2[middle_shape][objective_pos]);
         } else {
           directions = calculate_spocks_hands_direction(i, j, array_objective[objective_pos]);
         }
@@ -241,37 +254,42 @@ static bool update_time() {
   time_t now = time(NULL);
   struct tm *time = localtime(&now);
   int hour = time->tm_hour;
-  int new_min = -1;
+  int new_min1 = -1;
   int new_sec = -1;
   bool res = false;
 
-  if(clock_is_24h_style() == false && hour > 12) {
+  if(!clock_is_24h_style() && hour > 12) {
     hour = hour - 12;
   }
   hour10 = hour / 10;
   hour1 = hour % 10;
   
-  // Animate with special patterns every once in a while
-  if (min10 != time->tm_min / 10) {
-  //if (min1 != time->tm_min % 10) {
-    middleShape = rand() % NUM_SHAPES;
-    numMiddleAnimation = maxNumMiddleAnimation;
-  }
   min10 = time->tm_min / 10;
-  
-  new_min = time->tm_min % 10;
-  if(min1 != new_min) {
-    min1 = new_min;
+  new_min1 = time->tm_min % 10;
+  if(min1 != new_min1) {
+    min1 = new_min1;
     res = true;
   }
   
-  if(is_sec_shown) {
+  if(is_sec_shown || (is_shake_active && sec_show_on_shake)) {
     new_sec = time->tm_sec % 5;
     new_sec = (time->tm_sec - new_sec) / 5;
     if (sec != new_sec) {
       sec = new_sec;
       res = true;
     }
+  }
+  
+  // Animate with special patterns every once in a while
+  if ((animation_rate == ANIMATE_EVERY_1M && time->tm_sec == 0) ||
+      (animation_rate == ANIMATE_EVERY_10M && time->tm_min % 10 == 0 && time->tm_sec == 0) ||
+      (animation_rate == ANIMATE_EVERY_15M && time->tm_min % 15 == 0 && time->tm_sec == 0) ||
+      (animation_rate == ANIMATE_EVERY_30M && time->tm_min % 30 == 0 && time->tm_sec == 0) ||
+      (animation_rate == ANIMATE_EVERY_1H && time->tm_min == 0 && time->tm_sec == 0)
+     ) {
+  //if (min1 != time->tm_min % 10) {
+    middle_shape = rand() % NUM_SHAPES;
+    current_middle_animation = MAX_ANIMATIONS;
   }
   
   return res;
@@ -300,6 +318,53 @@ static void batt_handler(BatteryChargeState charge) {
   batt_level = charge.charge_percent / 10;
   if(is_batt_shown) {
     layer_mark_dirty(window_get_root_layer(my_window));
+  }
+}
+
+static void end_shake_timer(void *data) {
+  shake_timer = NULL;
+  is_shake_active = false;
+  if(sec_show_on_shake && !is_sec_shown) {
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  }
+  layer_mark_dirty(window_get_root_layer(my_window));
+}
+
+static void shake_handler(AccelAxisType axis, int32_t direction) {
+  switch(axis) {
+  case ACCEL_AXIS_X:
+    if (direction > 0) {
+      INFO("X>0");
+    } else {
+      INFO("X<0");
+    }
+    is_shake_active = true;
+    if (batt_show_on_shake) {
+      batt_level = battery_state_service_peek().charge_percent / 10;
+    }
+    if (bt_show_on_shake) {
+      is_bt_connected = bluetooth_connection_service_peek();
+    }
+    if(sec_show_on_shake) {
+      tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+    }
+    layer_mark_dirty(window_get_root_layer(my_window));
+    shake_timer = app_timer_register(shake_timer_delay, end_shake_timer, NULL);
+    break;
+  case ACCEL_AXIS_Y:
+    if (direction > 0) {
+      INFO("Y>0");
+    } else {
+      INFO("Y<0");
+    }
+    break;
+  case ACCEL_AXIS_Z:
+    if (direction > 0) {
+      INFO("Z>0");
+    } else {
+      INFO("Z<0");
+    }
+    break;
   }
 }
 
@@ -343,29 +408,34 @@ static void spock_layer_update_proc(Layer *layer, GContext *ctx) {
   //Boolean to help with the animation
   bool stop_animation = true;
   
-  if(numMiddleAnimation > 0) {
+  if(current_middle_animation > 0) {
     // Draw the full screen with some geometric pattern
-    //stop_animation = !draw_array(ctx, -1, -1, -1, -1, SHAPES2X2[middleShape], 2, 2, color_spocks_h10) && stop_animation;
+    //stop_animation = !draw_array(ctx, -1, -1, -1, -1, SHAPES2X2[middle_shape], 2, 2, color_spocks_h10) && stop_animation;
     //!!! Because of the color options we need to draw it element by element !!!
     ///// NUMBERS:
     stop_animation = !draw_array(ctx, 1, 0, NUMBER_WIDTH, NUMBER_HEIGHT,
-                                 SHAPES2X2[middleShape], true, 2, 2, color_spocks_h10) && stop_animation;
+                                 SHAPES2X2[middle_shape], true, 2, 2, color_spocks_h10) && stop_animation;
     stop_animation = !draw_array(ctx, NUMBER_WIDTH + 1, 0, NUMBER_WIDTH, NUMBER_HEIGHT,
-                                 SHAPES2X2[middleShape], true, 2, 2, color_spocks_h1) && stop_animation;
+                                 SHAPES2X2[middle_shape], true, 2, 2, color_spocks_h1) && stop_animation;
     stop_animation = !draw_array(ctx, 1, NUMBER_HEIGHT, NUMBER_WIDTH, NUMBER_HEIGHT,
-                                 SHAPES2X2[middleShape], true, 2, 2, color_spocks_m10) && stop_animation;
+                                 SHAPES2X2[middle_shape], true, 2, 2, color_spocks_m10) && stop_animation;
     stop_animation = !draw_array(ctx, NUMBER_WIDTH + 1, NUMBER_HEIGHT, NUMBER_WIDTH, NUMBER_HEIGHT,
-                                 SHAPES2X2[middleShape], true, 2, 2, color_spocks_m1) && stop_animation;
+                                 SHAPES2X2[middle_shape], true, 2, 2, color_spocks_m1) && stop_animation;
     ///// FIRST COLUMN:
-    stop_animation = !draw_array(ctx, 0, 0, 1, -1, SHAPES2X2[middleShape], true,
+    stop_animation = !draw_array(ctx, 0, 0, 1, -1, SHAPES2X2[middle_shape], true,
                                  2, 2, color_spocks_bt) && stop_animation;
     ///// LAST COLUMN:
     stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, 0, 1, -1,
-                                 SHAPES2X2[middleShape], true, 2, 2, color_spocks_sec) && stop_animation;
+                                 SHAPES2X2[middle_shape], true, 2, 2, color_spocks_sec) && stop_animation;
   } else {
     //Updating the time (hours and minutes)
-    stop_animation = !draw_array(ctx, 1, 0, NUMBER_WIDTH, NUMBER_HEIGHT, NUMBERS[hour10], false, 
-                                 NUMBER_WIDTH, NUMBER_HEIGHT, color_spocks_h10) && stop_animation;
+    if (show_zero_for_hours || hour10 != 0) {
+      stop_animation = !draw_array(ctx, 1, 0, NUMBER_WIDTH, NUMBER_HEIGHT, NUMBERS[hour10], false, 
+                                  NUMBER_WIDTH, NUMBER_HEIGHT, color_spocks_h10) && stop_animation;
+    } else {
+      stop_animation = !draw_array(ctx, 1, 0, NUMBER_WIDTH, NUMBER_HEIGHT, SHAPES2X2[middle_shape], true, 
+                                  NUMBER_WIDTH, NUMBER_HEIGHT, color_spocks_h10) && stop_animation;
+    }
     
     stop_animation = !draw_array(ctx, NUMBER_WIDTH + 1, 0, NUMBER_WIDTH, NUMBER_HEIGHT, NUMBERS[hour1], false,
                                  NUMBER_WIDTH, NUMBER_HEIGHT, color_spocks_h1) && stop_animation;
@@ -377,7 +447,7 @@ static void spock_layer_update_proc(Layer *layer, GContext *ctx) {
                                  NUMBER_WIDTH, NUMBER_HEIGHT, color_spocks_m1) && stop_animation;
     
     //Updating the seconds
-    if(is_sec_shown) {
+    if(is_sec_shown || (is_shake_active && sec_show_on_shake)) {
       //Corner at the beginning
       stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, SPOCKS_SCREEN_HEIGHT - 1, 1, 1,
                                    SECONDS1X1[0], false, 1, 1, color_spocks_sec) && stop_animation;
@@ -397,20 +467,20 @@ static void spock_layer_update_proc(Layer *layer, GContext *ctx) {
         }
       }
     } else {
-      stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, 0, 1, -1, NULL,
-                                   false, 0, 0, color_spocks_sec) && stop_animation;
+      stop_animation = !draw_array(ctx, SPOCKS_SCREEN_WIDTH - 1, 0, 1, -1, SHAPES2X2[middle_shape],
+                                   true, 2, 2, color_spocks_sec) && stop_animation;
     }
     
     //Updating the bluetooth status
-    if(is_bt_shown) {
+    if(is_bt_shown || (is_shake_active && bt_show_on_shake)) {
       stop_animation = !draw_array(ctx, 0, 0, 1, 2, is_bt_connected ? BLUETOOTH2X1[1] : BLUETOOTH2X1[0],
                                    false, 1, 2, color_spocks_bt) && stop_animation;
     } else {
-      stop_animation = !draw_array(ctx, 0, 0, 1, 2, NULL, false, 0, 0, color_spocks_bt) && stop_animation;
+      stop_animation = !draw_array(ctx, 0, 0, 1, 2, SHAPES2X2[middle_shape], true, 2, 2, color_spocks_bt) && stop_animation;
     }
     
     //Updating the battery level
-    if(is_batt_shown) {
+    if(is_batt_shown || (is_shake_active && batt_show_on_shake)) {
       if(batt_level < 2) {
         //First corner, if the battery is really low it has special ways to show it.
         stop_animation = !draw_array(ctx, 0, SPOCKS_SCREEN_HEIGHT - 1, 1, 1, BATTERY1X1[batt_level], false,
@@ -431,16 +501,16 @@ static void spock_layer_update_proc(Layer *layer, GContext *ctx) {
                                      1, 1, color_spocks_bt) && stop_animation;
       }
     } else {
-      stop_animation = !draw_array(ctx, 0, 2, 1, -1, NULL, false, 0, 0, color_spocks_bt) && stop_animation;
+      stop_animation = !draw_array(ctx, 0, 2, 1, -1, SHAPES2X2[middle_shape], true, 2, 2, color_spocks_bt) && stop_animation;
     }
     
   }
   
-  if(!stop_animation || numMiddleAnimation > 0) {
+  if(!stop_animation || current_middle_animation > 0) {
     if (stop_animation) {
-      numMiddleAnimation--;
-      if(numMiddleAnimation > 0) {
-        middleShape = rand() % NUM_SHAPES;
+      current_middle_animation--;
+      if(current_middle_animation > 0) {
+        middle_shape = rand() % NUM_SHAPES;
       }
     }
     animation_timer = app_timer_register(timer_delay, (AppTimerCallback) animation_callback, NULL);
@@ -531,15 +601,29 @@ static void bg_update_proc(Layer *layer, GContext *ctx) {
 
 static void window_load(Window *window) {
   LOG("Window load");
-  is_bt_shown = false; //grab from mem
+  //Grab all these values from permanent memory
+  is_bt_shown = false;
   is_bt_connected = bluetooth_connection_service_peek();
-  is_batt_shown = false; //grab from mem
+  is_batt_shown = false;
   batt_level = battery_state_service_peek().charge_percent / 10;
-  is_sec_shown = true; //grab from mem
-  //grab from mem the colors
+  is_sec_shown = false;
+  
+  bt_show_on_shake = true;
+  batt_show_on_shake = true;
+  sec_show_on_shake = true;
+
+  show_zero_for_hours = false;
+  
+  animation_rate = ANIMATE_EVERY_30M;
+  
+  hand_length = NORMAL_LENGTH_HAND_SPOCK;
+
   set_colors();
   
-  middleShape = rand() % NUM_SHAPES;
+  middle_shape = rand() % NUM_SHAPES;
+  
+  update_time();
+  current_middle_animation = MAX_ANIMATIONS;
   
   // Register with TickTimerService
   if(is_sec_shown) 
@@ -562,8 +646,8 @@ static void window_load(Window *window) {
 
 static void window_unload(Window *window) {
   LOG("Window unload");
-  layer_destroy(bg_layer);
-  layer_destroy(spock_layer);
+  layer_destroy_safe(bg_layer);
+  layer_destroy_safe(spock_layer);
 }
 
 void handle_init(void) {
@@ -582,20 +666,17 @@ void handle_init(void) {
   init_centers_array();
   init_spocks_array();
   
-  //Subscribe to BluetoothConnectionService and BatteryStateService
+  //Subscribe to BluetoothConnectionService and BatteryStateService and AccelerometerService
   bluetooth_connection_service_subscribe(bt_handler);
   battery_state_service_subscribe(batt_handler);
-  
-  // Make sure the time is displayed from the start
-  numMiddleAnimation = maxNumMiddleAnimation;
-  update_time();
+  accel_tap_service_subscribe(shake_handler);
 }
 
 void handle_deinit(void) {
   LOG("DEINIT");
   bluetooth_connection_service_unsubscribe();
   battery_state_service_unsubscribe();
-  window_destroy(my_window);
+  window_destroy_safe(my_window);
 }
 
 int main(void) {
